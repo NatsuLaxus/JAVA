043f064bd44f858fcb0ee960c88bcb0f8ef4a3d9
NatsuGrey <chiranjeevsinha.98@gmail.com>
2020-12-28 14:12:05 +0530

    webLogProgram

---
+++ WebLogProgram/LogAnalyzer.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class LogAnalyzer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+import edu.duke.*;
+
+public class LogAnalyzer
+{
+     private ArrayList<LogEntry> records;
+     
+     public LogAnalyzer() {
+         // complete constructor
+         records = new ArrayList<LogEntry>();
+     }
+        
+     public void readFile(String filename) {
+         // complete method
+         FileResource fr = new FileResource(filename);
+         WebLogParser wp = new WebLogParser();
+         for(String s: fr.lines())
+         {
+             LogEntry temp = wp.parseEntry(s);
+             records.add(temp);
+         }
+     }
+        
+     public void printAll() {
+         for (LogEntry le : records) {
+             System.out.println(le);
+         }
+     }
+     
+     
+}
